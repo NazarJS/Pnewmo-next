21bb7dc2cee26dbd99328d34d7dea861d19a802b
NazarJS <nazar.poluyanow@gmail.com>
2026-06-03 14:21:41 +0300

    add next

---
+++ .next/dev/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"OSVJSff2v6mlgaFCpy4WEQqOxDcoW7e0todNmF+m7DY=\"\n}"
---
+++ .next/dev/static/chunks/src_app_page_tsx_002ye8~._.js
@@ -0,0 +1,5 @@
+(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: ["static/chunks/node_modules_next_0fye-fd._.js","static/chunks/src_widgets_header_ui_0~2q-zo._.js","static/chunks/src_widgets_header_0u~t2xi._.css"],
+    source: "dynamic"
+});
