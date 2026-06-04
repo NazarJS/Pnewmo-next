c8baa00e53abacc049e63d44cc2aa2ab364ea656
Baldezhnik232 <stanislav.bondarev.2015@gmail.com>
2026-06-04 21:38:13 +0300

    making AWD without .next/

--- .next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"0R3ZuOR+u/eRnd/m8gibqIWkpDm/Zce1q/sokV21sb4=\"\n}"
--- .next/dev/static/chunks/12ep_next_app_056dt.2.js
+++
@@ -1,8 +0,0 @@
-(globalThis["TURBOPACK"] || (globalThis["TURBOPACK"] = [])).push([typeof document === "object" ? document.currentScript : undefined,
-"[project]/node_modules/.pnpm/next@16.2.6_@babel+core@7.29.7_react-dom@19.2.6_react@19.2.6__react@19.2.6_sass@1.100.0/node_modules/next/app.js [client] (ecmascript)", ((__turbopack_context__, module, exports) => {
-
-module.exports = __turbopack_context__.r("[project]/node_modules/.pnpm/next@16.2.6_@babel+core@7.29.7_react-dom@19.2.6_react@19.2.6__react@19.2.6_sass@1.100.0/node_modules/next/dist/pages/_app.js [client] (ecmascript)");
-}),
-]);
-
-//# sourceMappingURL=12ep_next_app_056dt.2.js.map
