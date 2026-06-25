122c5fca91d86fec9172c34db3baad57e3e2b595
NazarJS <nazar.poluyanow@gmail.com>
2026-06-25 12:35:59 +0300

    pull

--- .next/dev/static/chunks/_0rqeker._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/[turbopack]_browser_dev_hmr-client_hmr-client_ts_0wph3y~._.js","static/chunks/12ep_next_dist_compiled_next-devtools_index_0mqc7it.js","static/chunks/12ep_next_dist_compiled_react-dom_0m3sz-2._.js","static/chunks/12ep_next_dist_compiled_react-server-dom-turbopack_03cs~fc._.js","static/chunks/12ep_next_dist_compiled_0.a0att._.js","static/chunks/12ep_next_dist_client_09drmcz._.js","static/chunks/12ep_next_dist_0h94n6l._.js","static/chunks/0i4a_@swc_helpers_cjs_0hvz.20._.js"],
-    source: "entry"
-});
